b43f978b52c0d500527dd3585f21666c20505baf
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-09-14 11:54:36 +0000

    style: fix indent

--- app/web-app.py
+++ app/web-app.py
@@ -14,9 +14,9 @@ model = joblib.load(model_file)
 vectorizer = joblib.load(vectorizer_file)
 
 st.set_page_config(
-    page_title="MTS 45 Wiradesa: Berikan saranmu",
-    page_icon=icon_file,
-    initial_sidebar_state="expanded"
+  page_title="MTS 45 Wiradesa: Berikan saranmu",
+  page_icon=icon_file,
+  initial_sidebar_state="expanded"
 )
 
 st.title("Berikan saranmu disini!")
@@ -25,22 +25,22 @@ st.subheader('MTS 45 Wiradesa')
 popup_placeholder = st.empty()
 
 input_text = st.text_area(
-    label="Saran", 
-    label_visibility="hidden", 
-    placeholder="Tuliskan saranmu disini..."
+  label="Saran", 
+  label_visibility="hidden", 
+  placeholder="Tuliskan saranmu disini..."
 )
 
 if st.button(
-    label="Kirim", 
-    type="primary",
-    use_container_width=True
+  label="Kirim", 
+  type="primary",
+  use_container_width=True
 ):
-    if not input_text.strip():
-        popup_placeholder.error("Tolong untuk mengisi form saran terlebih dahulu sebelum mengirimnya.")
-    elif len(input_text) < 12:
-        popup_placeholder.error("Saran harus terdiri dari minimal 12 huruf.")
-    else:
-        popup_placeholder.success("Terimakasih telah memberikan saran, kami akan memprosesnya segera.")
-        st.write("Hasil:")
-        predict = model.predict(vectorizer.transform([input_text]))
-        st.write(predict)
+  if not input_text.strip():
+    popup_placeholder.error("Tolong untuk mengisi form saran terlebih dahulu sebelum mengirimnya.")
+  elif len(input_text) < 12:
+    popup_placeholder.error("Saran harus terdiri dari minimal 12 huruf.")
+  else:
+    popup_placeholder.success("Terimakasih telah memberikan saran, kami akan memprosesnya segera.")
+    st.write("Hasil:")
+    predict = model.predict(vectorizer.transform([input_text]))
+    st.write(predict)

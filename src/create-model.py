71a48cef13acfcb32f5efb4f8769df4665d04679
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-10-30 10:57:14 +0000

    fix: change value test size

--- src/create-model.py
+++ src/create-model.py
@@ -20,7 +20,7 @@ try:
   vectorizer = TfidfVectorizer()
   X = vectorizer.fit_transform(X_text)
       
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
   model = MultinomialNB()
   model.fit(X_train, y_train)
   
